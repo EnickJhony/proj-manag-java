7092c482106b24c70aff1936408bc249ecf4f5ce
Enick Jhony <101206404+EnickJhony@users.noreply.github.com>
2024-01-31 13:39:17 -0400

    refactor: adding validation to `entities`

--- proj-manag/src/main/java/br/dev/enick/projmanag/modules/member/MemberEntity.java
+++ proj-manag/src/main/java/br/dev/enick/projmanag/modules/member/MemberEntity.java
@@ -1,11 +1,21 @@
 package br.dev.enick.projmanag.modules.member;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
+@Entity(name = "member")
 public class MemberEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Long id;
+
+  @NotBlank
   private String name;
   
 }
--- proj-manag/src/main/java/br/dev/enick/projmanag/modules/project/ProjectEntity.java
+++ proj-manag/src/main/java/br/dev/enick/projmanag/modules/project/ProjectEntity.java
@@ -1,21 +1,36 @@
 package br.dev.enick.projmanag.modules.project;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Entity(name = "project")
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
 public class ProjectEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Long id;
+
   @NotBlank
   private String name;
+
   @NotBlank
   private String description;
+  
   @NotBlank
   private String startDate;
   private String endDate;
   private String members;
-  @NotBlank
   private Integer memberId;
   
 }
